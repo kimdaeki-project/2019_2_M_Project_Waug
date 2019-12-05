18a3e0f730f613e8f819caef34eec2e5ac35928b
chlgpgus57 <55524992+chlgpgus57@users.noreply.github.com>
2019-12-05 09:52:41 +0900

    junit

---
+++ src/test/java/com/wg/p1/testAbstractCase.java
@@ -0,0 +1,14 @@
+package com.wg.p1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAbstractCase {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
