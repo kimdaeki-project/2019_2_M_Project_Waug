5a0e3cc1ff55751d278d6e1519e572fd5d75126f
Parkbb <pyj9088@gmail.com>
2019-12-08 19:32:48 +0900

    123

---
+++ src/main/java/com/wg/p1/dao/ReviewDAO.java
@@ -0,0 +1,21 @@
+package com.wg.p1.dao;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import com.wg.p1.model.ReviewVO;
+
+@Repository
+public class ReviewDAO {
+	
+	@Inject
+	private SqlSession sqlSession;
+	private final static String NAMESPACE="reviewMapper.";
+	
+	public int reviewWrite(ReviewVO reviewVO) throws Exception{
+		
+		return sqlSession.insert(NAMESPACE+"reviewInsert", reviewVO);
+	}
+}
---
+++ src/main/java/com/wg/p1/model/ReviewVO.java
@@ -0,0 +1,77 @@
+package com.wg.p1.model;
+
+public class ReviewVO {
+	
+	private int rv_num;
+	private int goods_num;
+	private String rv_title;
+	private String rv_writer;
+	private String rv_contents;
+	private String rv_reg_date;
+	private int rv_ref;
+	private int rv_step;
+	private int rv_depth;
+	private int rv_score;
+	
+	
+	public int getRv_num() {
+		return rv_num;
+	}
+	public void setRv_num(int rv_num) {
+		this.rv_num = rv_num;
+	}
+	public int getGoods_num() {
+		return goods_num;
+	}
+	public void setGoods_num(int goods_num) {
+		this.goods_num = goods_num;
+	}
+	public String getRv_title() {
+		return rv_title;
+	}
+	public void setRv_title(String rv_title) {
+		this.rv_title = rv_title;
+	}
+	public String getRv_writer() {
+		return rv_writer;
+	}
+	public void setRv_writer(String re_writer) {
+		this.rv_writer = rv_writer;
+	}
+	public String getRv_contents() {
+		return rv_contents;
+	}
+	public void setRv_contents(String rv_contents) {
+		this.rv_contents = rv_contents;
+	}
+	public String getRv_reg_date() {
+		return rv_reg_date;
+	}
+	public void setRv_reg_date(String rv_reg_date) {
+		this.rv_reg_date = rv_reg_date;
+	}
+	public int getRv_ref() {
+		return rv_ref;
+	}
+	public void setRv_ref(int rv_ref) {
+		this.rv_ref = rv_ref;
+	}
+	public int getRv_step() {
+		return rv_step;
+	}
+	public void setRv_step(int rv_step) {
+		this.rv_step = rv_step;
+	}
+	public int getRv_depth() {
+		return rv_depth;
+	}
+	public void setRv_depth(int rv_depth) {
+		this.rv_depth = rv_depth;
+	}
+	public int getRv_score() {
+		return rv_score;
+	}
+	public void setRv_score(int rv_score) {
+		this.rv_score = rv_score;
+	}
+}
