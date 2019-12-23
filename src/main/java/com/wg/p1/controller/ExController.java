acf2cf4e97bc19a10af5da1733dc77f64ab083c4
Parkbb <pyj9088@gmail.com>
2019-12-23 15:33:54 +0900

    1

---
+++ src/main/java/com/wg/p1/controller/ExController.java
@@ -0,0 +1,25 @@
+package com.wg.p1.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ExController {
+	
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ModelAndView getClient() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("error");
+		return mv;
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView getEx() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("error");
+		return mv;
+	}
+}
